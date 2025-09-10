50bdaa4c331fbd8082cd78ca01bf0f57fd3c772e
JuanPabloA1 <juan.mera.agudelo@correounivalle.edu.co>
2025-09-10 18:50:12 -0500

    Cors environment updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://ticket-backend-production-36b2.up.railway.app/api', // Tu endpoint de API de producción
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://ticket-backend-production-36b2.up.railway.app/api',
   apiKey: 'tu_api_key_de_produccion' // Tu clave secreta de producción
 };
